6c8ca0ca0d9a923bd0f13e1f4afec091622bfd99
Dore Zahavy <dorezahavy@gmail.com>
2023-06-21 11:12:00 +0300

    img load on editor

--- js/meme-gen.control.js
+++ js/meme-gen.control.js
@@ -1,1 +1,106 @@
 'use strict'
+
+const gElCanvas = document.querySelector('.meme-canvas')
+const gCtx = gElCanvas.getContext('2d')
+const gElGallery = document.querySelector('.image-gallery')
+const gElEditor = document.querySelector('.meme-editor')
+
+const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
+
+function onInit(){
+    console.log('init!');
+    resizeCanvas()
+    addListeners()
+    gElGallery.classList.add('show')
+}
+
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+    window.addEventListener('resize', resizeCanvas)
+    // gElCanvas.addEventListener('click',draw())
+}
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
+
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchend', onUp)
+}
+
+function onDown(ev) {
+    // const pos = getEvPos(ev)
+    // gLastPos = pos
+    // setIsPainting(true)
+    // console.log('pos:', pos)
+    // if (!getTool().brush) draw(pos)
+}
+
+function onMove(ev) {
+    // if (!getIsPainting() || !getTool().brush) return
+
+    // const pos = getEvPos(ev)
+    // draw(pos)
+    // gLastPos = pos
+}
+
+function onUp() {
+    // setIsPainting(false)
+    // if (getTool().brush) gLineCount = 0
+    // document.body.style.cursor = 'grab'
+}
+
+function getEvPos(ev) {
+
+    let pos = {
+        x: ev.offsetX,
+        y: ev.offsetY,
+    }
+
+    if (TOUCH_EVS.includes(ev.type)) {
+        // Prevent triggering the mouse ev
+        ev.preventDefault()
+        // Gets the first touch point
+        ev = ev.changedTouches[0]
+        // Calc the right pos according to the touch screen
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+        }
+    }
+    return pos
+}
+
+function renderImg(img) {
+    console.log('img:', img)
+    // Draw the img on the canvas
+    gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function resizeCanvas() {
+    console.log('hi');
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+}
+
+function onOpenEditor(img,imgId){
+    console.log('img:', img)
+    gElGallery.classList.remove('show')
+    gElEditor.classList.add('show')
+    resizeCanvas()
+    setMeme(imgId)
+    renderMeme(img)
+    // renderImg(img)
+}
+
+function renderMeme(img){
+    const meme = getMeme()
+    gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
--- js/services/meme-gen.service.js
+++ js/services/meme-gen.service.js
@@ -1,1 +1,45 @@
 'use strict'
+
+var gImgs = [
+    { id: 1, url: 'img/1.jpg', keywords: ['politics', ''] },
+    { id: 2, url: 'img/2.jpg', keywords: ['friends', 'animals'] },
+    { id: 3, url: 'img/3.jpg', keywords: ['friends', 'animals'] },
+    { id: 4, url: 'img/4.jpg', keywords: ['animals', 'cats'] },
+    { id: 5, url: 'img/5.jpg', keywords: ['kids', 'cat'] },
+    { id: 6, url: 'img/6.jpg', keywords: ['science', 'crazy'] },
+    { id: 7, url: 'img/7.jpg', keywords: ['kids', ''] },
+    { id: 8, url: 'img/8.jpg', keywords: ['movie', ''] },
+    { id: 9, url: 'img/9.jpg', keywords: ['kids', 'funny'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['politics', 'funny'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['', 'friends'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['', ''] },
+    { id: 13, url: 'img/13.jpg', keywords: ['movies', 'cool'] },
+    { id: 14, url: 'img/14.jpg', keywords: ['movies', 'cool'] },
+    { id: 15, url: 'img/15.jpg', keywords: ['movies', ''] },
+    { id: 16, url: 'img/16.jpg', keywords: ['movies', 'funny'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['politics', 'cool'] },
+    { id: 18, url: 'img/18.jpg', keywords: ['movies', 'friends'] }
+]
+
+var gMeme = {
+    selectedImgId: 0,
+    selectedLineIdx: 1,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
+
+var gKeywordSearchCountMap = { 'funny': 0, 'cat': 0, 'baby': 0 }
+
+
+function setMeme(imgId){
+gMeme.selectedImgId = imgId
+}
+
+function getMeme(){
+    return gMeme
+}
