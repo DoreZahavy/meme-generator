b4d222ac9c94a7ea277a7e7cc7cb2deb33285b34
Dore Zahavy <dorezahavy@gmail.com>
2023-06-21 09:57:34 +0300

    initial structuring

---
+++ js/services/util.service.js
@@ -0,0 +1,64 @@
+'use strict'
+
+function makeId(length = 6) {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var txt = ''
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return txt
+}
+
+function makeLorem(wordCount = 100) {
+    const words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
+    var txt = ''
+    while (wordCount > 0) {
+        wordCount--
+        txt += words[Math.floor(Math.random() * words.length)] + ' '
+    }
+    return txt
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('')
+    var color = '#'
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+function createShuffledArray(size) {
+    var nums = []
+    for (var i = 1; i <= size; i++) {
+        nums.push(i)
+    }
+    return shuffle(nums)
+}
+
+function shuffle(items) {
+    var randIdx, keep;
+    for (var i = items.length - 1; i > 0; i--) {
+        randIdx = getRandomIntInclusive(0, items.length - 1);
+        keep = items[i];
+        items[i] = items[randIdx];
+        items[randIdx] = keep;
+    }
+    return items;
+}
+
+function deepCopy(obj){
+    return JSON.parse(JSON.stringify(obj))
+}
